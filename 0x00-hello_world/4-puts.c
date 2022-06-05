2d207f8b16482c7e2f1905653280f564eaa46112
S-A-oghene <agbamogunjacob.com>
2022-06-05 07:15:28 -0700

    4-puts.c 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)/*C program that prints Programming is like building a multilingual puzzle*/
 {
 
-        puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
