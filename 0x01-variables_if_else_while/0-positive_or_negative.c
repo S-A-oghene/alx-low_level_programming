316600c35b3c0d52cdaf92178b6756f6a4876f16
S-A-oghene <agbamogunjacob.com>
2022-09-21 13:06:10 -0700

    4th commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * This program assigns a random number to the variable n each time it is executed.
- *based a conditiom
- *Return : always 0
+/**
+*main - Entry point
+*Description: assigns a random number to the variable n
+*based a conditiom
+*Return: always 0
 */
 int main(void)
 {
